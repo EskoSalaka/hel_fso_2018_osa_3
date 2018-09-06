535e629e2d9a0cf9b86971f27f54a1b1dec4194a
Esko Salaka <esko.salaka@aalto.fi>
2018-09-06 11:24:28 +0300

    Heroku
    Heroku 1

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ app.put('/api/persons/:id', (req, res) => {
 });
 
 module.exports = app;
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
